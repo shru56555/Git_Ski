7b2288c015de9360136bdf946a37c9da07cc84fb
shru56555 <shruti@maguresoftwares.com>
2020-06-05 14:48:07 +0530

    Comments removed

--- Learn.js
+++ Learn.js
@@ -1,8 +1,5 @@
 Console.log("Hello World A");
 Console.log("Hello World B");
-<<<<<<< HEAD
 Console.log("Hello World Branch A");
 Console.log("Hello World Branch B");
-=======
 Console.log("Hello World C");
->>>>>>> 89a2f48... C
